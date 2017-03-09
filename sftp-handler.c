dfbcf00fda4a8011521a2a5eb6ffa5ae49799657
sk3l <mike@skelton.onl>
2017-03-09 08:18:12 -0500

    First functional custom SFTP handler (lstat) posting to the event FIFO.

--- sftp-handler.c
+++ sftp-handler.c
@@ -3,11 +3,15 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "log.h"
+#include "sshbuf.h"
 #include "sftp.h"
 #include "sftp-handler.h"
 #include "xmalloc.h"
+
+extern struct sshbuf * iqueue;
 
 static int fd_fifo;
 static const char * path_fifo = "/tmp/sftp_evts";
@@ -17,7 +21,7 @@ static void post_lstat_to_fifo(u_int32_t id);
 
 static struct sftp_handler overrides[] = {
    {"stat fifo", NULL, SSH2_FXP_STAT, post_stat_to_fifo, 0},
-   {"lstat fifo", NULL, SSH2_FXP_STAT, post_lstat_to_fifo, 0}
+   {"lstat fifo", NULL, SSH2_FXP_LSTAT, post_lstat_to_fifo, 0}
 };
 
 int init_handler_overrides(handler_tbl handlers)
@@ -26,22 +30,22 @@ int init_handler_overrides(handler_tbl handlers)
                new_hptr = NULL;
 
    u_int32_t handler_size = sizeof(struct sftp_handler);
-   
+
    logit("Initializing custom handler overrides.");
 
    fd_fifo = open(path_fifo, O_WRONLY | O_NONBLOCK);
    if (fd_fifo < 0)
    {
       error("Encountered error opening SFTP event FIFO: %d", errno);
-      return 0; 
-   }    
+      return 0;
+   }
 
-   /* Append event handlers of interest */
+   /* Insert event handlers of interest */
    hptr = handlers[SSH2_FXP_LSTAT];
 
-   new_hptr = (handler_ptr) xcalloc(handler_size, 3);   
-   memcpy(new_hptr, hptr, handler_size);
-   memcpy(&new_hptr[1], &overrides[1], handler_size);
+   new_hptr = (handler_ptr) xcalloc(handler_size, 3);
+   memcpy(&new_hptr[0], &overrides[1], handler_size);
+   memcpy(&new_hptr[1], hptr, handler_size);
 
    handlers[SSH2_FXP_LSTAT] = new_hptr;
    free(hptr);
@@ -56,5 +60,26 @@ void post_stat_to_fifo(u_int32_t id)
 
 void post_lstat_to_fifo(u_int32_t id)
 {
+   int rc = 0;
+   size_t len = 0;
+   const u_char * path = NULL;
+
    logit("Processing custom handler override for lstat.");
+
+   rc = sshbuf_peek_string_direct(iqueue, &path, &len);
+   if (rc != 0)
+   {
+      error("Encountered error during SSH buff peek in post_lstat_to_fifo: %d", rc);
+      return;
+   }
+
+	debug("Dispatch lstat name \"%s\" to event FIFO.", path);
+
+   rc = write(fd_fifo, path, len);
+   if (rc < 1)
+   {
+      error("Encountered error during FIFO write in post_lstat_to_fifo: %d", errno);
+      return;
+   }
+
 }
