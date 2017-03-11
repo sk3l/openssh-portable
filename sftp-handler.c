609e7009b377865c549a7661c65beaac9b75f080
sk3l <mike@skelton.onl>
2017-03-11 16:52:55 -0500

    Refactor SFTP server to locate event handlers via array subscript (instead of iterative scan). Add function stubs for every SFTP request type in sftp-handler.c

--- sftp-handler.c
+++ sftp-handler.c
@@ -16,70 +16,189 @@ extern struct sshbuf * iqueue;
 static int fd_fifo;
 static const char * path_fifo = "/tmp/sftp_evts";
 
+static void post_open_to_fifo(u_int32_t id);
+static void post_close_to_fifo(u_int32_t id);
+static void post_read_to_fifo(u_int32_t id);
+static void post_write_to_fifo(u_int32_t id);
 static void post_stat_to_fifo(u_int32_t id);
 static void post_lstat_to_fifo(u_int32_t id);
+static void post_fstat_to_fifo(u_int32_t id);
+static void post_setstat_to_fifo(u_int32_t id);
+static void post_fsetstat_to_fifo(u_int32_t id);
+static void post_opendir_to_fifo(u_int32_t id);
+static void post_readdir_to_fifo(u_int32_t id);
+static void post_remove_to_fifo(u_int32_t id);
+static void post_mkdir_to_fifo(u_int32_t id);
+static void post_rmdir_to_fifo(u_int32_t id);
+static void post_realpath_to_fifo(u_int32_t id);
+static void post_rename_to_fifo(u_int32_t id);
+static void post_readlink_to_fifo(u_int32_t id);
+static void post_symlink_to_fifo(u_int32_t id);
 
-static struct sftp_handler overrides[] = {
-   {"stat fifo", NULL, SSH2_FXP_STAT, post_stat_to_fifo, 0},
-   {"lstat fifo", NULL, SSH2_FXP_LSTAT, post_lstat_to_fifo, 0}
+struct sftp_handler overrides[] = {
+	   { NULL, NULL, 0, NULL, 0 },
+	   { NULL, NULL, 0, NULL, 0 },
+	   { NULL, NULL, 0, NULL, 0 },
+	   /* NB. SSH2_FXP_OPEN does the readonly check in the handler itself */
+	   { "open to fifo", NULL, SSH2_FXP_OPEN, post_open_to_fifo, 0 },
+	   { "close to fifo", NULL, SSH2_FXP_CLOSE, post_close_to_fifo, 0 },
+	   { "read to fifo", NULL, SSH2_FXP_READ, post_read_to_fifo, 0 },
+	   { "write to fifo", NULL, SSH2_FXP_WRITE, post_write_to_fifo, 1 },
+	   { "lstat to fifo", NULL, SSH2_FXP_LSTAT, post_lstat_to_fifo, 0 },
+	   { "fstat to fifo", NULL, SSH2_FXP_FSTAT, post_fstat_to_fifo, 0 },
+	   { "setstat to fifo", NULL, SSH2_FXP_SETSTAT, post_setstat_to_fifo, 1 },
+	   { "fsetstat to fifo", NULL, SSH2_FXP_FSETSTAT, post_fsetstat_to_fifo, 1 },
+	   { "opendir to fifo", NULL, SSH2_FXP_OPENDIR, post_opendir_to_fifo, 0 },
+	   { "readdir to fifo", NULL, SSH2_FXP_READDIR, post_readdir_to_fifo, 0 },
+	   { "remove to fifo", NULL, SSH2_FXP_REMOVE, post_remove_to_fifo, 1 },
+	   { "mkdir to fifo", NULL, SSH2_FXP_MKDIR, post_mkdir_to_fifo, 1 },
+	   { "rmdir to fifo", NULL, SSH2_FXP_RMDIR, post_rmdir_to_fifo, 1 },
+	   { "realpath to fifo", NULL, SSH2_FXP_REALPATH, post_realpath_to_fifo, 0 },
+	   { "stat to fifo", NULL, SSH2_FXP_STAT, post_stat_to_fifo, 0 },
+	   { "rename to fifo", NULL, SSH2_FXP_RENAME, post_rename_to_fifo, 1 },
+	   { "readlink to fifo", NULL, SSH2_FXP_READLINK, post_readlink_to_fifo, 0 },
+	   { "print symlink to fifo", NULL, SSH2_FXP_SYMLINK, post_symlink_to_fifo, 1 },
 };
 
-int init_handler_overrides(handler_tbl handlers)
+int init_handler_overrides(handler_list * htbl)
 {
-   handler_ptr     hptr = NULL,
-               new_hptr = NULL;
+	u_int i;
+	handler_list   hlist = NULL,
+	           new_hlist = NULL;
 
-   u_int32_t handler_size = sizeof(struct sftp_handler);
+	logit("Initializing custom handler overrides.");
 
-   logit("Initializing custom handler overrides.");
+	fd_fifo = open(path_fifo, O_WRONLY | O_NONBLOCK);
+	if (fd_fifo < 0)
+	{
+	   error("Encountered error opening SFTP event FIFO: %d", errno);
+	   return 0;
+	}
 
-   fd_fifo = open(path_fifo, O_WRONLY | O_NONBLOCK);
-   if (fd_fifo < 0)
-   {
-      error("Encountered error opening SFTP event FIFO: %d", errno);
-      return 0;
-   }
+	/* Insert event handlers overrides*/
+	for (i = 0; i <= SSH2_FXP_CNT; i++) {
+	   if (overrides[i].handler == NULL)
+	      continue;
 
-   /* Insert event handlers of interest */
-   hptr = handlers[SSH2_FXP_LSTAT];
+	   hlist = htbl[i];
 
-   new_hptr = (handler_ptr) xcalloc(handler_size, 3);
-   memcpy(&new_hptr[0], &overrides[1], handler_size);
-   memcpy(&new_hptr[1], hptr, handler_size);
+	   new_hlist = (handler_list) xcalloc(sizeof(handler_ptr), 3);
+	   new_hlist[0] = &overrides[i];
+	   new_hlist[1] = hlist[0];
 
-   handlers[SSH2_FXP_LSTAT] = new_hptr;
-   free(hptr);
-
-   return 1;
+	   htbl[i] = new_hlist;
+	   free(hlist);
+	}
+	return 1;
 }
 
-void post_stat_to_fifo(u_int32_t id)
+static void post_open_to_fifo(u_int32_t id)
 {
-   logit("Processing custom handler override for stat.");
+	logit("Processing custom handler override for open.");
 }
 
-void post_lstat_to_fifo(u_int32_t id)
+static void post_close_to_fifo(u_int32_t id)
 {
-   int rc = 0;
-   size_t len = 0;
-   const u_char * path = NULL;
+	logit("Processing custom handler override for close.");
+}
 
-   logit("Processing custom handler override for lstat.");
+static void post_read_to_fifo(u_int32_t id)
+{
+	logit("Processing custom handler override for read.");
+}
 
-   rc = sshbuf_peek_string_direct(iqueue, &path, &len);
-   if (rc != 0)
-   {
-      error("Encountered error during SSH buff peek in post_lstat_to_fifo: %d", rc);
-      return;
-   }
+static void post_write_to_fifo(u_int32_t id)
+{
+	logit("Processing custom handler override for write.");
+}
+
+static void post_stat_to_fifo(u_int32_t id)
+{
+	logit("Processing custom handler override for stat.");
+}
+
+static void post_lstat_to_fifo(u_int32_t id)
+{
+	int rc = 0;
+	size_t len = 0;
+	const u_char * path = NULL;
+
+	logit("Processing custom handler override for lstat.");
+
+	rc = sshbuf_peek_string_direct(iqueue, &path, &len);
+	if (rc != 0)
+	{
+	   error("Encountered error during SSH buff peek in post_lstat_to_fifo: %d", rc);
+	   return;
+	}
 
 	debug("Dispatch lstat name \"%s\" to event FIFO.", path);
 
-   rc = write(fd_fifo, path, len);
-   if (rc < 1)
-   {
-      error("Encountered error during FIFO write in post_lstat_to_fifo: %d", errno);
-      return;
-   }
+	rc = write(fd_fifo, path, len);
+	if (rc < 1)
+	{
+	   error("Encountered error during FIFO write in post_lstat_to_fifo: %d", errno);
+	   return;
+	}
 
+}
+
+static void post_fstat_to_fifo(u_int32_t id)
+{
+	logit("Processing custom handler override for fstat.");
+}
+
+static void post_setstat_to_fifo(u_int32_t id)
+{
+	logit("Processing custom handler override for setstat.");
+}
+
+static void post_fsetstat_to_fifo(u_int32_t id)
+{
+	logit("Processing custom handler override for fsetstat.");
+}
+
+static void post_opendir_to_fifo(u_int32_t id)
+{
+	logit("Processing custom handler override for opendir.");
+}
+
+static void post_readdir_to_fifo(u_int32_t id)
+{
+	logit("Processing custom handler override for readdir.");
+}
+
+static void post_remove_to_fifo(u_int32_t id)
+{
+	logit("Processing custom handler override for remove.");
+}
+
+static void post_mkdir_to_fifo(u_int32_t id)
+{
+	logit("Processing custom handler override for mkdir.");
+}
+
+static void post_rmdir_to_fifo(u_int32_t id)
+{
+	logit("Processing custom handler override for rmdir.");
+}
+
+static void post_realpath_to_fifo(u_int32_t id)
+{
+	logit("Processing custom handler override for realpath.");
+}
+
+static void post_rename_to_fifo(u_int32_t id)
+{
+	logit("Processing custom handler override for rename.");
+}
+
+static void post_readlink_to_fifo(u_int32_t id)
+{
+	logit("Processing custom handler override for readlink.");
+}
+
+static void post_symlink_to_fifo(u_int32_t id)
+{
+	logit("Processing custom handler override for symlink.");
 }
--- sftp-server.c
+++ sftp-server.c
@@ -84,8 +84,6 @@ struct Stat {
 	Attrib attrib;
 };
 
-#define HANDLER_CNT SSH2_FXP_SYMLINK+1
-struct sftp_handler ** handler_table;
 
 /* Packet handlers */
 static void process_open(u_int32_t id);
@@ -113,38 +111,39 @@ static void process_extended_hardlink(u_int32_t id);
 static void process_extended_fsync(u_int32_t id);
 static void process_extended(u_int32_t id);
 
-//struct sftp_handler {
-//	const char *name;	/* user-visible name for fine-grained perms */
-//	const char *ext_name;	/* extended request name */
-//	u_int type;		/* packet type, for non extended packets */
-//	void (*handler)(u_int32_t);
-//	int does_write;		/* if nonzero, banned for readonly mode */
-//};
-
 struct sftp_handler handlers[] = {
-	/* NB. SSH2_FXP_OPEN does the readonly check in the handler itself */
-	{ "open", NULL, SSH2_FXP_OPEN, process_open, 0 },
-	{ "close", NULL, SSH2_FXP_CLOSE, process_close, 0 },
-	{ "read", NULL, SSH2_FXP_READ, process_read, 0 },
-	{ "write", NULL, SSH2_FXP_WRITE, process_write, 1 },
-	{ "lstat", NULL, SSH2_FXP_LSTAT, process_lstat, 0 },
-	{ "fstat", NULL, SSH2_FXP_FSTAT, process_fstat, 0 },
-	{ "setstat", NULL, SSH2_FXP_SETSTAT, process_setstat, 1 },
-	{ "fsetstat", NULL, SSH2_FXP_FSETSTAT, process_fsetstat, 1 },
-	{ "opendir", NULL, SSH2_FXP_OPENDIR, process_opendir, 0 },
-	{ "readdir", NULL, SSH2_FXP_READDIR, process_readdir, 0 },
-	{ "remove", NULL, SSH2_FXP_REMOVE, process_remove, 1 },
-	{ "mkdir", NULL, SSH2_FXP_MKDIR, process_mkdir, 1 },
-	{ "rmdir", NULL, SSH2_FXP_RMDIR, process_rmdir, 1 },
-	{ "realpath", NULL, SSH2_FXP_REALPATH, process_realpath, 0 },
-	{ "stat", NULL, SSH2_FXP_STAT, process_stat, 0 },
-	{ "rename", NULL, SSH2_FXP_RENAME, process_rename, 1 },
-	{ "readlink", NULL, SSH2_FXP_READLINK, process_readlink, 0 },
-	{ "symlink", NULL, SSH2_FXP_SYMLINK, process_symlink, 1 },
-	{ NULL, NULL, 0, NULL, 0 }
+      { NULL, NULL, 0, NULL, 0 },
+      { NULL, NULL, 0, NULL, 0 },
+      { NULL, NULL, 0, NULL, 0 },
+      /* NB. SSH2_FXP_OPEN does the readonly check in the handler itself */
+      { "open", NULL, SSH2_FXP_OPEN, process_open, 0 },
+      { "close", NULL, SSH2_FXP_CLOSE, process_close, 0 },
+      { "read", NULL, SSH2_FXP_READ, process_read, 0 },
+      { "write", NULL, SSH2_FXP_WRITE, process_write, 1 },
+      { "lstat", NULL, SSH2_FXP_LSTAT, process_lstat, 0 },
+      { "fstat", NULL, SSH2_FXP_FSTAT, process_fstat, 0 },
+      { "setstat", NULL, SSH2_FXP_SETSTAT, process_setstat, 1 },
+      { "fsetstat", NULL, SSH2_FXP_FSETSTAT, process_fsetstat, 1 },
+      { "opendir", NULL, SSH2_FXP_OPENDIR, process_opendir, 0 },
+      { "readdir", NULL, SSH2_FXP_READDIR, process_readdir, 0 },
+      { "remove", NULL, SSH2_FXP_REMOVE, process_remove, 1 },
+      { "mkdir", NULL, SSH2_FXP_MKDIR, process_mkdir, 1 },
+      { "rmdir", NULL, SSH2_FXP_RMDIR, process_rmdir, 1 },
+      { "realpath", NULL, SSH2_FXP_REALPATH, process_realpath, 0 },
+      { "stat", NULL, SSH2_FXP_STAT, process_stat, 0 },
+      { "rename", NULL, SSH2_FXP_RENAME, process_rename, 1 },
+      { "readlink", NULL, SSH2_FXP_READLINK, process_readlink, 0 },
+      { "symlink", NULL, SSH2_FXP_SYMLINK, process_symlink, 1 },
 };
 
+handler_list handler_table [SSH2_FXP_CNT];
 
+static void init_handler_type(u_int type) {
+   if (handlers[type].handler != NULL) {
+      handler_table[type] = (handler_list) xcalloc(sizeof(handler_ptr), 2);
+      handler_table[type][0] = &handlers[type];
+   }
+}
 
 /* SSH2_FXP_EXTENDED submessages */
 struct sftp_handler extended_handlers[] = {
@@ -1409,8 +1408,9 @@ process(void)
 	u_int consumed;
 	u_char type;
 	const u_char *cp;
-	int i, j, r;
+	int i, r;
 	u_int32_t id;
+   handler_list hlist;
 
 	buf_len = sshbuf_len(iqueue);
 	if (buf_len < 5)
@@ -1448,25 +1448,20 @@ process(void)
 		if ((r = sshbuf_get_u32(iqueue, &id)) != 0)
 			fatal("%s: buffer error: %s", __func__, ssh_err(r));
 
-      for (i = 0; i < HANDLER_CNT; i++) {
-			if (handler_table[i] == NULL)
-            continue;
+      if (type < SSH2_FXP_CNT) {
+         hlist = handler_table[type];
 
-         if (type == handler_table[i]->type) {
-            /* Execute every handler in the chain for this table entry. */
-            for (j = 0; handler_table[i][j].handler != NULL; ++j) {
-				   if (!request_permitted(&handler_table[i][j])) {
-					   send_status(id,
+         /* Execute every handler in the list for this table entry. */
+         for (i = 0; hlist[i] != NULL; ++i) {
+				if (!request_permitted(hlist[i])) {
+			      send_status(id,
 					    SSH2_FX_PERMISSION_DENIED);
 				   }  else   {
-					   handler_table[i][j].handler(id);
+					   hlist[i]->handler(id);
 				   }
 			   }
-            goto ENDSCAN;
          }
-		}
-ENDSCAN:
-		if (handlers[i].handler == NULL)
+     else
 			error("Unknown message %u", type);
 	}
 	/* discard the remaining bytes from the current packet */
@@ -1529,6 +1524,19 @@ sftp_server_main(int argc, char **argv, struct passwd *user_pw)
 
 	pw = pwcopy(user_pw);
 
+   /* test */
+   sleep(30);
+
+   /* Initialize the handler table with the default packet handlers */
+   for (i = 0; i <= SSH2_FXP_CNT; i++) {
+      init_handler_type(i);
+   } 
+
+#ifdef HAVE_HANDLER_OVERRIDES
+   /* Initialize application overrides over the SFTP handlers table. */
+   init_handler_overrides(handler_table);
+#endif
+
 	while (!skipargs && (ch = getopt(argc, argv,
 	    "d:f:l:P:p:Q:u:cehR")) != -1) {
 		switch (ch) {
@@ -1537,8 +1545,13 @@ sftp_server_main(int argc, char **argv, struct passwd *user_pw)
 				fprintf(stderr, "Invalid query type\n");
 				exit(1);
 			}
-			for (i = 0; handlers[i].handler != NULL; i++)
-				printf("%s\n", handlers[i].name);
+			for (i = 0; i < SSH2_FXP_CNT; i++) {
+				for (len = 0; 
+                (handler_table[i] != NULL) && (handler_table[i][len] != NULL);
+                 len++) {
+               printf("%s\n", handler_table[i][len]->name);
+            }
+         }
 			for (i = 0; extended_handlers[i].handler != NULL; i++)
 				printf("%s\n", extended_handlers[i].name);
 			exit(0);
@@ -1653,22 +1666,6 @@ sftp_server_main(int argc, char **argv, struct passwd *user_pw)
 			    strerror(errno));
 		}
 	}
-
-   /* test */
-   sleep(30);
-
-   handler_table = (handler_tbl) xcalloc(sizeof(handler_ptr), HANDLER_CNT);
-
-   /* Initialize the handler table with the default packet handlers */
-   for (i = 0; handlers[i].handler != NULL; i++) {
-      handler_table[handlers[i].type] = (handler_ptr) xcalloc(sizeof(struct sftp_handler), 2);
-      memcpy(handler_table[handlers[i].type], &handlers[i], sizeof(struct sftp_handler));
-   }
-
-#ifdef HAVE_HANDLER_OVERRIDES
-   /* Initialize application overrides over the SFTP handlers table. */
-   init_handler_overrides(handler_table);
-#endif
 
 	set_size = howmany(max + 1, NFDBITS) * sizeof(fd_mask);
 	for (;;) {
