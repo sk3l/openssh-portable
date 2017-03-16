97ca54dffd7d88e6aaea89d63f981ddfb8bbb844
sk3l <mike@skelton.onl>
2017-03-16 09:07:57 -0400

    Move SFTP override _request_ handler logic to separate modules.

---
+++ sftp-request-handler.h
@@ -0,0 +1,25 @@
+#ifndef SFTP_REQUEST_HANDLER_H
+#define SFTP_REQUEST_HANDLER_H
+
+#include "includes.h"
+#include <dirent.h>
+
+/* redeclaration from sftp-server.c (need to make a common module)*/
+typedef struct Handle Handle;
+struct Handle {
+	int use;
+	DIR *dirp;
+	int fd;
+	int flags;
+	char *name;
+	u_int64_t bytes_read, bytes_write;
+	int next_unused;
+};
+
+enum {
+	HANDLE_UNUSED,
+	HANDLE_DIR,
+	HANDLE_FILE
+};
+
+#endif
