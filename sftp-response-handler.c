2cccf54a44d82a4e821946c63ff4d0b08dae2e73
sk3l <mike@skelton.onl>
2017-03-30 14:01:22 -0400

    Rename 'fifo' to 'sink' in SFTP request & response handler modules.

--- sftp-response-handler.c
+++ sftp-response-handler.c
@@ -18,12 +18,12 @@
 
 extern struct sshbuf * oqueue;
 
-static char buff_fifo[SFTP_MAX_MSG_LENGTH]; /* max SFTP packet size */
+static char buff_sink[SFTP_MAX_MSG_LENGTH]; /* max SFTP packet size */
 
-static void post_response_to_fifo(u_int32_t id);
+static void post_response_to_sink(u_int32_t id);
 
 struct sftp_handler rsp_overrides[] = {
-	   { "response to fifo", NULL, SSH2_FXP_OPEN, post_response_to_fifo, 0 }
+	   { "response to sink", NULL, SSH2_FXP_OPEN, post_response_to_sink, 0 }
 };
 
 static const char * rsp_strings [] = {
@@ -37,7 +37,7 @@ static const char * rsp_strings [] = {
 
 static size_t RESPONSE_OFFSET = sizeof(u_int) + sizeof(char) + sizeof(u_int);
 
-static void post_response_to_fifo(u_int32_t id)
+static void post_response_to_sink(u_int32_t id)
 {
 	int rc = 0;
 	size_t len = 0;
@@ -57,7 +57,7 @@ static void post_response_to_fifo(u_int32_t id)
    len = get_u32(resp_ptr);
    if (len < 1)
    {
-	   error("Encountered empty SFTP response message in post_response_to_fifo");
+	   error("Encountered empty SFTP response message in post_response_to_sink");
 	   return;
    }
 
@@ -75,11 +75,11 @@ static void post_response_to_fifo(u_int32_t id)
       rc = get_ssh_string(resp_ptr + RESPONSE_OFFSET + (sizeof(u_int)), &resp_msg, &mlen);
 	   if (rc != 0)
 	   {
-	      error("Encountered error during SSH buff peek in post_response_to_fifo: %d", rc);
+	      error("Encountered error during SSH buff peek in post_response_to_sink: %d", rc);
 	      return;
 	   }
 
-      len = sprintf(buff_fifo, "id=%-10d resp=%-10s msg='%.*s'\n", id, resp_str, (int)mlen, resp_msg);
+      len = sprintf(buff_sink, "id=%-10d resp=%-10s msg='%.*s'\n", id, resp_str, (int)mlen, resp_msg);
    }
    else if (resp_type == SSH2_FXP_HANDLE)
    {
@@ -89,15 +89,15 @@ static void post_response_to_fifo(u_int32_t id)
       rc = get_ssh_string(resp_ptr + RESPONSE_OFFSET, &resp_msg, &mlen);
 	   if (rc != 0)
 	   {
-	      error("Encountered error during SSH buff peek in post_response_to_fifo: %d", rc);
+	      error("Encountered error during SSH buff peek in post_response_to_sink: %d", rc);
 	      return;
 	   }
 
 	   hptr = get_sftp_handle(resp_msg, mlen, -1);
 		if (hptr != NULL) {
-         len = sprintf(buff_fifo, "id=%-10d resp=%-10s path='%s'\n", id, resp_str, hptr->name);
+         len = sprintf(buff_sink, "id=%-10d resp=%-10s path='%s'\n", id, resp_str, hptr->name);
       } else {
-	      error("Encountered error in post_close_to_fifo: bad handle");
+	      error("Encountered error in post_close_to_sink: bad handle");
          return;
 	   }
    }
@@ -108,7 +108,7 @@ static void post_response_to_fifo(u_int32_t id)
 
       mlen = get_u32(resp_ptr + RESPONSE_OFFSET);
 
-      len = sprintf(buff_fifo, "id=%-10d resp=%-10s len=%d\n", id, resp_str, mlen);
+      len = sprintf(buff_sink, "id=%-10d resp=%-10s len=%d\n", id, resp_str, mlen);
    }
    else if (resp_type == SSH2_FXP_NAME)
    {
@@ -117,24 +117,24 @@ static void post_response_to_fifo(u_int32_t id)
 
       mlen = get_u32(resp_ptr + RESPONSE_OFFSET);
 
-      len = sprintf(buff_fifo, "id=%-10d resp=%-10s cnt=%d\n", id, resp_str, mlen);
+      len = sprintf(buff_sink, "id=%-10d resp=%-10s cnt=%d\n", id, resp_str, mlen);
    }
    else if (resp_type == SSH2_FXP_ATTRS)
    {
       resp_type -= 100;
       resp_str = rsp_strings[resp_type];
-      len = sprintf(buff_fifo, "id=%-10d resp=%-10s\n", id, resp_str);
+      len = sprintf(buff_sink, "id=%-10d resp=%-10s\n", id, resp_str);
    }
    else
 	{
-	   error("Encountered unknown SFTP response type %d in post_response_to_fifo", resp_type);
+	   error("Encountered unknown SFTP response type %d in post_response_to_sink", resp_type);
 	   return;
 	}
 
-	rc = write_to_handler_sink(buff_fifo, len);
+	rc = write_to_handler_sink(buff_sink, len);
 	if (rc < 1)
 	{
-	   error("Encountered error during write to SFTP handler sink in post_open_to_fifo: %d", errno);
+	   error("Encountered error during write to SFTP handler sink in post_open_to_sink: %d", errno);
 	}
 }
 
