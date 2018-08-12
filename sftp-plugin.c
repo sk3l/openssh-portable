18a19ce0f72c615a8e8c91eb42a9328be603c901
sk3l <mike@skelton.onl>
2018-08-12 17:46:39 -0400

    SFTP plugin WIP #2

--- sftp-plugin.c
+++ sftp-plugin.c
@@ -1,14 +1,19 @@
+#include <ctype.h>
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "sshbuf.h"
 #include "sftp-plugin.h"
 
 #include "log.h"
 #include "pathnames.h"
+#include "xmalloc.h"
 
 #define SFTP_PLUGIN_CONF SSHDIR "/sftp_plugin_conf"
 
-static Plugin * plugins_;
+static struct sshbuf * fbuf;
+
+static Plugin * plugins;
 
 const char * PLUGIN_SEQUENCE_STR [] = {
    "BEFORE",
@@ -24,12 +29,63 @@ static enum PLUGIN_SEQUENCE plugin_sequence_str_to_enum(const char * sequence)
 }
 
 // Read & parse the SFTP plugin conf
-static void load_plugin_conf()
+static int load_plugin_conf()
 {
+	char * line = NULL, * lpos;
+	size_t linesize = 0, linecnt = 0;
+   int i, rc;
+   FILE * fconf;
+
+   if ((fconf = fopen(SFTP_PLUGIN_CONF, "r")) == NULL)
+   {
+      perror(SFTP_PLUGIN_CONF);
+      exit(1);
+   } 
+
+   while (getline(&line, &linesize, fconf) != -1)
+   {
+      ++linecnt;
+      lpos = line;
+
+      // Skip whitespace
+      for (i = 0; i < linesize; ++i)
+         if (!isspace(*lpos++))
+               break;
+
+      // Skip empty lines, comment lines
+      if ((i == linesize) || (*lpos == '#'))
+         continue;
+
+      // Stash the line in our buffer
+      if ((rc = sshbuf_put(fbuf, lpos, linesize-i)) != 0)
+			fatal("%s: buffer error", __func__);
+   }
+
+   free(line);
+   fclose(fconf);
+
+   return linecnt;
+}
+
+// Scan the config buffer, initializing the plugin list
+static void init_plugins(int count)
+{
+   u_int buflen;
+   const u_char * pluginpos;
+   int i, pluginidx;
+
+   plugins = (Plugin*) xcalloc(sizeof(Plugin), count);
+
+   buflen    = sshbuf_len(fbuf);
+   pluginpos = sshbuf_ptr(fbuf);
+   pluginidx = 0;
+  
+   // Pattern of the plugin conf should be:
+   // <pluginname> <whitespace>+ [<sequence>] \n 
 
 }
 
-// Open & scan the SFTP plugin shared objects
+// Open & scan the shared objects for the plugins
 static void load_plugin_so()
 {
 
@@ -37,7 +93,14 @@ static void load_plugin_so()
 
 int sftp_plugins_init()
 {
-   load_plugin_conf();
+   int count;
+
+   if ((fbuf = sshbuf_new()) == NULL)
+		fatal("%s: sshbuf_new failed", __func__);
+
+   count = load_plugin_conf();
+
+   init_plugins(count);
 
    load_plugin_so();
 
@@ -46,6 +109,10 @@ int sftp_plugins_init()
 
 int sftp_plugins_release()
 {
+   sshbuf_free(fbuf);
+
+   free(plugins);
+
    return 0;
 }
 
