24259f818a7fb2bdf0fd6fc0b1d720b44d1d49b7
sk3l <mike@skelton.onl>
2018-09-09 21:01:19 -0400

    SFTP plugin WIP #8

--- sftp-callback.h
+++ sftp-callback.h
@@ -59,18 +59,7 @@ struct sftp_callbacks {
   sftp_cbk_attrs     pf_attrs;
 };
 
-//typedef int (*sftp_callback_func)(const char *, uint32_t);
-
-//struct sftp_callback {
-//	const char *name_;	         /* descriptive name for log print */
-//   int type_;		               /* packet type, for non extended packets */
-//   sftp_callback_func callback_; /* the actual plugin function to exexute */
-//};
-
 typedef struct sftp_callbacks * callbacks_ptr;
 
-//callback_ptr create_sftp_callback(const char * name, int type, sftp_callback_func cbkfunc);
-
-//void destroy_sftp_callback(callback_ptr pcbk);
 
 #endif
